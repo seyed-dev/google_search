db6199191976c64bf1c41af9384405eb63162b30
SeYeD <16540000+seyed-dev@users.noreply.github.com>
2022-10-15 22:59:22 +0330

    Create google_search.py

---
+++ google_search.py
@@ -0,0 +1,70 @@
+import requests
+import urllib
+from requests_html import HTMLSession
+
+
+def get_source(url):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+def scrape_google(query):
+
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.com/search?q=" + query)
+
+    links = list(response.html.absolute_links)
+    google_domains = ('https://www.google.', 
+                      'https://google.', 
+                      'https://webcache.googleusercontent.', 
+                      'http://webcache.googleusercontent.', 
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.')
+
+    for url in links[:]:
+        if url.startswith(google_domains):
+            links.remove(url)
+
+    return links
+
+
+def get_results(query):
+    
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.co.uk/search?q=" + query)
+    
+    return response
+
+def parse_results(response):
+    
+    css_identifier_result = ".tF2Cxc"
+    css_identifier_title = "h3"
+    css_identifier_link = ".yuRUbf a"
+    css_identifier_text = ".VwiC3b"
+    css_identifier_date = ".WZ8Tjf"
+    
+    results = response.html.find(css_identifier_result)
+
+    output = []
+    
+    for result in results:
+        try:
+            item = {
+                'title': result.find(css_identifier_title, first=True).text,
+                'link': result.find(css_identifier_link, first=True).attrs['href'],
+                'text': result.find(css_identifier_text, first=True).text,
+                'time': result.find(css_identifier_date, first=True).text
+            }
+            output.append(item)
+        except Exception as e:
+            print(e)
+        
+    return output
+
+def google_search_api(query):
+    response = get_results(query)
+    return parse_results(response)
